900466033d18fa1292dcbd6f6bab0f185151648a
fbkl <frkle@hvl.no>
2026-07-06 16:30:23 +0200

    doesnt even run now

--- src/osrt_ros/UIMU/UIMUnode.cpp
+++ src/osrt_ros/UIMU/UIMUnode.cpp
@@ -353,8 +353,8 @@ void UIMUnode::onInit()
 void UIMUnode::run() {
 
 	ROS_DEBUG_STREAM("started to run");
-	//ros::AsyncSpinner spinner(4);
-	//spinner.start();
+	ros::AsyncSpinner spinner(4);
+	spinner.start();
 	bool keep_running=true;
 	int ij = 0;
 	int runs_to_log= 4;
